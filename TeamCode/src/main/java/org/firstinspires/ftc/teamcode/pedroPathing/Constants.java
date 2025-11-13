30f963abbcad8128317a03921a56a430d5cb6852
Aarav <athakur@dohacollege.com.qa>
2025-11-13 17:26:03 +0300

    Setup Pedro Pathing Added the PIDF add-ons to functions (Values TBD through tests)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,18 +1,56 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+public class Constants  {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(5)
+            .forwardZeroPowerAcceleration(0) // The value to be determined through testing Automatic --> Forward Zero Power Acceleration Tuner
+            .lateralZeroPowerAcceleration(0) // The value to be determined through testing Automatic --> Lateral Zero Power Acceleration Tuner
+            .translationalPIDFCoefficients(new PIDFCoefficients(0,0,0,0)) // The value to be determined through testing Manual --> Translational Tuner
+            .headingPIDFCoefficients(new PIDFCoefficients(0,0,0,0));// The value to be determined through testing Manual --> Heading Tuner
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("rf")
+            .rightRearMotorName("rr")
+            .leftRearMotorName("lr")
+            .leftFrontMotorName("lf")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(0) // The value to be determined through testing Automatic --> Forward Velocity Tuner
+            .yVelocity(0); // The value to be determined through testing Automatic --> Lateral Velocity Tuner
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(-5) // In inches
+            .strafePodX(0.5) // In inches
+            .distanceUnit(DistanceUnit.MM)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+    public static PathConstraints pathConstraints = new PathConstraints(0.99,
+            100,
+            1,
+            1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)
+                .mecanumDrivetrain(driveConstants)
                 .pathConstraints(pathConstraints)
                 .build();
     }
