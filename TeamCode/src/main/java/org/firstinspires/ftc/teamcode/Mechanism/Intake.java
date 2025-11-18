051faa67ae6f23a291039265659d9fb581568064
Aarav <athakur@dohacollege.com.qa>
2025-11-18 21:16:09 +0300

    Added new file for subsystems in a new package

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/Intake.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.Mechanism;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Intake {
+    DcMotorEx Intake;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/Shooter.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Mechanism;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Shooter {
+    DcMotorEx Shooter;
+    Servo Servoleft;
+    Servo Servoright;
+
+}
