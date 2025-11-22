ffa01fd7bdd273139d5e64d441d337cd60b65022
Aarav <athakur@dohacollege.com.qa>
2025-11-22 18:21:43 +0300

    Used new files and replaced individual motors COmpleted State machine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Full_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Full_Auto.java
@@ -8,16 +8,11 @@ import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
-import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.Mechanism.Gate;
+import org.firstinspires.ftc.teamcode.Mechanism.Intake;
+import org.firstinspires.ftc.teamcode.Mechanism.Localisation;
 import org.firstinspires.ftc.teamcode.Mechanism.Shooter;
 import org.firstinspires.ftc.teamcode.Mechanism.System_Init;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
@@ -26,24 +21,17 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 @Configurable // Panels
 public class Full_Auto extends OpMode {
 
-    System_Init System_Init = new System_Init();
+    System_Init system_init = new System_Init();
     Shooter shooter = new Shooter();
-    DcMotorEx backLeftDrive;
-    DcMotorEx backRightDrive;
-    DcMotorEx intake;
-    DcMotorEx shoot;
-    Servo Hoodleft;
-    Servo Hoodright;
-    Limelight3A limelight3A;
-    IMU imu;
+    Gate gate = new Gate();
+    Intake intake = new Intake();
+    Localisation localisation = new Localisation();
 
     private TelemetryManager panelsTelemetry; // Panels Telemetry instance
     public Follower follower; // Pedro Pathing follower instance
     private int pathState; // Current autonomous path state (state machine)
-
-    private double start_X;
-    private double start_Y;
-    private Pose startPose = new Pose(start_X, start_Y, Math.toRadians(180)); // Start Pose of our robot.
+    private int Pathnum = 2;
+    private Pose startPose; // Start Pose of our robot.
     private final Pose SmallscorePose = new Pose(56, 8, Math.toRadians(135)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
     private final Pose BigscorePose = new Pose(45, 98, Math.toRadians(135)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
 
@@ -56,14 +44,12 @@ public class Full_Auto extends OpMode {
     private final Pose pickup3Pose = new Pose(13, 86, Math.toRadians(0)); // Lowest (Third Set) of Artifacts from the Spike Mark.
     private final Pose pickup3PoseReverse = new Pose(34, 86, Math.toRadians(0)); // Highest (First Set) of Artifacts from the Spike Mark.
 
-    double currentX, currentY;
     private final Pose Goal = new Pose(5,144);
-    private  Pose Current = new Pose(5,144);
     double distance;
     @Override
     public void init() {
         panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
-        System_Init.init(hardwareMap);
+        system_init.init(hardwareMap);
         follower = Constants.createFollower(hardwareMap);
 
         panelsTelemetry.debug("Status", "Initialized");
@@ -72,41 +58,22 @@ public class Full_Auto extends OpMode {
 
     @Override
     public void init_loop() {
-        YawPitchRollAngles orientatiuon = imu.getRobotYawPitchRollAngles();
-        limelight3A.updateRobotOrientation(orientatiuon.getYaw(AngleUnit.DEGREES));
-        LLResult result = limelight3A.getLatestResult();
-        if (result != null) {
-            if (result.isValid()) {
-                start_X = result.getBotpose().getPosition().x;
-                start_Y = result.getBotpose().getPosition().y;
-            }
-        }
+        startPose = localisation.getCamPosition();
         follower.setStartingPose(startPose);
     }
 
     @Override
     public void loop() {
         follower.update(); // Update Pedro Pathing
-        pathState = autonomousPathUpdate(); // Update autonomous state machine
-
-        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-        limelight3A.updateRobotOrientation(orientation.getYaw(AngleUnit.DEGREES));
-        LLResult result = limelight3A.getLatestResult();
-        if (result != null) {
-            if (result.isValid()) {
-                currentX = result.getBotpose().getPosition().x;
-                currentY = result.getBotpose().getPosition().y;
-            }
+        try {
+            pathState = autonomousPathUpdate(); // Update autonomous state machine
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        follower.setStartingPose(startPose);
 
-        distance = Current.distanceFrom(Goal);
-        double servoPos = distance/1000;
-
-        Hoodleft.setPosition(servoPos);
-        Hoodright.setPosition(1-servoPos);
-        double shooterSpeed = shooter.SpeedCalc(distance);
-        shoot.setVelocity(shooterSpeed);
+        distance = follower.getPose().distanceFrom(Goal);
+        intake.Inhale();
+        shooter.SpeedCalc(distance);
 
 
         // Log values to Panels and Driver Station
@@ -129,7 +96,7 @@ public class Full_Auto extends OpMode {
                                 SmallscorePose
                         )
                 )
-                .setLinearHeadingInterpolation(imu.getRobotYawPitchRollAngles().getYaw(), 110)
+                .setLinearHeadingInterpolation(startPose.getHeading(), 110)
                 .build();
 
         Path1 = follower
@@ -213,15 +180,44 @@ public class Full_Auto extends OpMode {
         }
 
 
-    public int autonomousPathUpdate() {
+    public int autonomousPathUpdate() throws InterruptedException {
         switch (pathState){
             case 0:
                 follower.followPath(preload);
                 setPathState(1);
-                break;
+                if (!follower.isBusy()){
+                    break;
+                }
             case 1:
-
-
+                gate.open();
+                wait(1000);
+                gate.close();
+                setPathState(Pathnum);
+                Pathnum += 1;
+                break;
+            case 2:
+                follower.followPath(Path1);
+                setPathState(1);
+                if (!follower.isBusy()){
+                    break;
+                }
+            case 3:
+                follower.followPath(Path2);
+                setPathState(1);
+                if (!follower.isBusy()){
+                    break;
+                }
+            case 4:
+                follower.followPath(Path3);
+                setPathState(1);
+                if (!follower.isBusy()){
+                    break;
+                }
+            case 5:
+                follower.followPath(Path4);
+                if (!follower.isBusy()){
+                    break;
+                }
         }
         return pathState;
     }
