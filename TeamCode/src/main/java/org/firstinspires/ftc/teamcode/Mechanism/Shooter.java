9c17c3ca87cfd68f636340f819f9e2995974d4bc
Aarav <athakur@dohacollege.com.qa>
2025-11-22 18:20:55 +0300

    Used Shooter to adjust Hood aswell

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/Shooter.java
@@ -18,20 +18,25 @@ public class Shooter {
     double Kp_Shoot = Constants_TeleOp.Kp_Shoot, Kd_shoot = Constants_TeleOp.Kd_Shoot, Ki_shoot = Constants_TeleOp.Ki_Shoot, Kf_Shoot = Constants_TeleOp.Kf_Shoot;
 
 
-    public double SpeedCalc(double distance){
+    public void SpeedCalc(double distance){
         speed = 5*distance;
         hood = distance/10;
+        Servoleft.setPosition(hood);
+        Servoright.setPosition(hood);
+
         double dtS = Math.max(timerS.seconds(), 1e-6);
         double velocity = (Shooter.getCurrentPosition() - lastShooterPos)/ timerS.seconds();
         double errorS = speed - velocity;
+
         ShootErrorSum += errorS * dtS;
-        ShootErrorSum = Math.max(-5000, Math.min(5000, ShootErrorSum)); // keep clamped always
+        ShootErrorSum = Math.max(-5000, Math.min(5000, ShootErrorSum));
         double derivativeS = (errorS - lastShooterError) / dtS;
-        lastShooterError = errorS;
         double shoot = (Kp_Shoot * errorS) + (Kd_shoot * derivativeS) + (Ki_shoot * ShootErrorSum) + (Kf_Shoot * speed);
+
         Shooter.setVelocity(shoot);
+
+        lastShooterError = errorS;
         lastShooterPos = Shooter.getCurrentPosition();
         timerS.reset();
-        return shoot;
     }
 }
