12acb1d46ead02a251d1c14bd397296cc44fb001
Aarav <athakur@dohacollege.com.qa>
2025-11-22 18:19:33 +0300

    Actually used the intake file for commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/Intake.java
@@ -4,4 +4,12 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 public class Intake {
     DcMotorEx Intake;
+
+    public void Inhale(){
+        Intake.setPower(1);
+    }
+
+    public void Exhale(){
+        Intake.setPower(-1);
+    }
 }
