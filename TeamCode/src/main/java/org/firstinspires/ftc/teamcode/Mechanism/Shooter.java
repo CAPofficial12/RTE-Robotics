6880050cb19c363a7b2cfdb379eb0096360aaaea
Aarav <athakur@dohacollege.com.qa>
2025-12-19 18:42:55 +0300

    Updated shooter code to tune PIDF instead

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanism/Shooter.java
@@ -1,42 +1,79 @@
 package org.firstinspires.ftc.teamcode.Mechanism;
 
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
-public class Shooter {
-    DcMotorEx Shooter;
-    Servo Servoleft;
-    Servo Servoright;
-    double ShootErrorSum;
-    ElapsedTime timerS = new ElapsedTime();
-    double speed;
-    double hood;
-    double lastShooterError;
-    double lastShooterPos;
+@TeleOp(name = "Shooter Tuning")
+public class Shooter extends OpMode {
+    System_Init system_init = new System_Init();
 
-    double Kp_Shoot = Constants_TeleOp.Kp_Shoot, Kd_shoot = Constants_TeleOp.Kd_Shoot, Ki_shoot = Constants_TeleOp.Ki_Shoot, Kf_Shoot = Constants_TeleOp.Kf_Shoot;
+    double[] step = {100,10,1,0.1,0.01,0.001};
+    int a = 0;
+    int mode = 1;
+    double Kp = 0;
+    double Ki = 0;
+    double Kd = 0;
+    double Kf = 0;
+    double[] target_speeds = {1000,2000};
+    double target_speed = target_speeds[1];
 
+    @Override
+    public void init() {
+        system_init.init(hardwareMap);
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(Kp, Ki, Kd, Kf);
+        system_init.shooter.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        telemetry.addLine("Init Complete");
+    }
 
-    public void SpeedCalc(double distance){
-        speed = 5*distance;
-        hood = distance/10;
-        Servoleft.setPosition(hood);
-        Servoright.setPosition(hood);
+    @Override
+    public void loop() {
+        if (gamepad1.left_bumper){
+            target_speed = target_speeds[1];
+        } else if (gamepad1.right_bumper) {
+            target_speed = target_speeds[0];
+        }
+        
+        if (gamepad1.dpadUpWasPressed()){
+            a += 1;
+        } else if (gamepad1.dpadDownWasPressed()) {
+            a -= 1;
+        }
+        
+        if (gamepad1.dpad_right){
+            mode = 1;
+        } else if (gamepad1.dpad_left){
+            mode = -1;
+        }
+        
+        if (gamepad1.y) {
+            Kp += mode * step[a];
+        } else if (gamepad1.b) {
+            Ki += mode * step[a];
+        } else if (gamepad1.a) {
+            Kd += mode * step[a];
+        } else if (gamepad1.x) {
+            Kf += mode * step[a];
+        }
 
-        double dtS = Math.max(timerS.seconds(), 1e-6);
-        double velocity = (Shooter.getCurrentPosition() - lastShooterPos)/ timerS.seconds();
-        double errorS = speed - velocity;
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(Kp, Ki, Kd, Kf);
+        system_init.shooter.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        system_init.shooter.setVelocity(target_speed);
 
-        ShootErrorSum += errorS * dtS;
-        ShootErrorSum = Math.max(-5000, Math.min(5000, ShootErrorSum));
-        double derivativeS = (errorS - lastShooterError) / dtS;
-        double shoot = (Kp_Shoot * errorS) + (Kd_shoot * derivativeS) + (Ki_shoot * ShootErrorSum) + (Kf_Shoot * speed);
+        double currentVelocity = system_init.shooter.getVelocity();
+        double error =  target_speed - currentVelocity;
 
-        Shooter.setVelocity(shoot);
-
-        lastShooterError = errorS;
-        lastShooterPos = Shooter.getCurrentPosition();
-        timerS.reset();
+        telemetry.addData("Target Velocity", target_speed);
+        telemetry.addData("Current velocity", currentVelocity);
+        telemetry.addData("Error", error);
+        telemetry.addData("Mode", mode);
+        telemetry.addData("Step", step[a]);
+        telemetry.addLine("------------------------------------------");
+        telemetry.addData("Kp", Kp);
+        telemetry.addData("Ki", Ki);
+        telemetry.addData("Kd", Kd);
+        telemetry.addData("Kf", Kf);
+        telemetry.update();
     }
 }
